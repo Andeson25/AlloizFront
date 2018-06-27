30c829420b6b0b429ae8b967e752cb4c20d0003f
nazar.yurkiw <yurkiw.nazar@gmail.com>
2018-06-27 15:27:49 +0300

    About Us without Team

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,12 +3,14 @@ import { PagesComponent } from './pages/pages.component';
 import { HomeComponent } from './pages/home/home.component';
 import {GlobalImportsModule} from '../shared/config/global-imports.module';
 import {MenuComponent} from '../source/menu/menu.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 @NgModule({
   imports: [
     GlobalImportsModule
   ],
   declarations: [PagesComponent, HomeComponent,
-    MenuComponent]
+    MenuComponent,
+    AboutUsComponent]
 })
 export class PagesModule { }
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,13 +1,18 @@
 import {PagesComponent} from "./pages/pages.component";
 import {Routes} from "@angular/router";
 import {HomeComponent} from "./pages/home/home.component";
+import {AboutUsComponent} from './pages/about-us/about-us.component';
 
 export const pagesRoutes: Routes = [
   {
     path: '', component: PagesComponent ,children: [
       {
         path:'', component: HomeComponent
+      },
+      {
+        path:'about', component: AboutUsComponent
       }
+
     ]
   }
 ];
