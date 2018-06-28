1945695bed049e405d6734acb3535b07ea84219d
Lviv-code <palchinskiyv@gmail.com>
2018-06-28 17:09:50 +0300

    Add portfolio description

--- src/app/pages/cabinet/cabinet/add/add.module.ts
+++ src/app/pages/cabinet/cabinet/add/add.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { AddComponent } from './add/add.component';
 import {GlobalImportsModule} from '../../../../shared/config/global-imports.module';
 import { WorkerComponent } from './add/worker/worker.component';
+import { PortfolioDescriptionComponent } from './add/portfolio-description/portfolio-description.component';
 
 
 @NgModule({
   imports: [
     GlobalImportsModule
   ],
-  declarations: [AddComponent, WorkerComponent]
+  declarations: [AddComponent, WorkerComponent, PortfolioDescriptionComponent]
 })
 export class AddModule { }
---
+++ src/app/pages/cabinet/cabinet/add/add/portfolio-description/portfolio-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-description',
+  templateUrl: './portfolio-description.component.html',
+  styleUrls: ['./portfolio-description.component.css']
+})
+export class PortfolioDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
