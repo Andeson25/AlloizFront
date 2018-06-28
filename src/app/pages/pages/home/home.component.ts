ff9b27e5c5a6174da1f4eb18fc8d02b47573812d
Lviv-code <palchinskiyv@gmail.com>
2018-06-28 12:58:06 +0300

    Home component slider

--- src/app/pages/pages/home/home.component.ts
+++ src/app/pages/pages/home/home.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  index: number = 3;
+
   constructor() { }
 
   ngOnInit() {
+  }
+  scroll(event){
+    if(this.index>=2) {
+      event ? this.index -= 1 : this.index += 1;
+    }else if( this.index==1 && event==false) {
+      this.index += 1;
+    }
   }
 
 }
