9f71b9c854fec04d772e33e9d045435661b95240
Lviv-code <palchinskiyv@gmail.com>
2018-06-29 19:25:55 +0300

    Add update technology!

--- src/app/pages/cabinet/cabinet/update/update.routes.ts
+++ src/app/pages/cabinet/cabinet/update/update.routes.ts
@@ -1,9 +1,10 @@
 import {UpdateComponent} from './update.component';
 import {WorkerComponent} from './update/worker/worker.component';
 import {Routes} from '@angular/router';
-import {worker} from 'cluster';
 import {WorkerOneComponent} from './update/worker/worker-one/worker-one.component';
 import {IncumbencyComponent} from './update/incumbency/incumbency.component';
+import {TechnologyOneComponent} from "./update/technology/technology-one/technology-one.component";
+import {TechnologyComponent} from "./update/technology/technology.component";
 
 
 export const updateRoutes :Routes =[
@@ -17,7 +18,14 @@ export const updateRoutes :Routes =[
       },
       {
         path: 'incumbency', component: IncumbencyComponent
+      },
+      {
+        path: 'tecnology', component: TechnologyComponent
+      },
+      {
+        path: 'tecnology/:id', component: TechnologyOneComponent
       }
+
     ]
   }
 ]
