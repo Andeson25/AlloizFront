bce958b2ce432fb7d801efc83f6baf232e5f8958
Lviv-code <palchinskiyv@gmail.com>
2018-07-03 11:15:25 +0300

    Add callbackTopic to contact forms

--- src/app/pages/pages/contacts/contacts.component.ts
+++ src/app/pages/pages/contacts/contacts.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {CallbackTopicService} from "../../../shared/service/callback-topic.service";
+import {CallbackTopic} from "../../../shared/models/callback-topic";
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
+  providers:[CallbackTopicService]
 })
 export class ContactsComponent implements OnInit {
 
-  constructor() { }
+  calbackTopics: CallbackTopic[] =[];
+  constructor(private _callbackTopic: CallbackTopicService) {
+    this._callbackTopic.findAllAvailable().subscribe(next=>{
+      this.calbackTopics=next;
+    }, error=>{
+      console.log(error)
+    },()=>{
+      console.log(this.calbackTopics);
+    })
+  }
 
   ngOnInit() {
   }
