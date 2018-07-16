75eabbff04e6750de0809cd04931c3e8e07a55a1
DanyloZasymenko <danik1492@gmail.com>
2018-07-16 13:52:25 +0300

    changed url

--- src/app/shared/config/url.ts
+++ src/app/shared/config/url.ts
@@ -1,2 +1,2 @@
-export const url = "http://192.168.0.127:8080";
-// export const url = "http://192.168.0.127:9090";
+// export const url = "http://192.168.0.127:8080";
+export const url = "http://192.168.0.127:9090";
