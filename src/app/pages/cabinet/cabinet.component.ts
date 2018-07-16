abce09858e90f069dc4efa13534054935c901116
vetalolon <vetalolon@gmail.com>
2018-07-16 15:59:36 +0300

    button logout

--- src/app/pages/cabinet/cabinet.component.ts
+++ src/app/pages/cabinet/cabinet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-cabinet',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CabinetComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) { }
+
+  logOut(){
+    localStorage.clear();
+    this._router.navigateByUrl('/login');
+  }
 
   ngOnInit() {
   }
